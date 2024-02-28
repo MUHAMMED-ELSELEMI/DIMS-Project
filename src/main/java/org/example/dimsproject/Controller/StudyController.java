150f29853cfd1f09d14d8bc5b89ee5493fa0e4ff
ismail beli <ismail.beli@ogr.sakarya.edu.tr>
2024-02-28 22:22:47 +0300

    StudyController is done.

--- src/main/java/org/example/dimsproject/Controller/StudyController.java
+++ src/main/java/org/example/dimsproject/Controller/StudyController.java
@@ -2,10 +2,10 @@ package org.example.dimsproject.Controller;
 
 import javafx.fxml.FXML;
 import javafx.event.ActionEvent;
-import javafx.scene.control.Label;
-import javafx.scene.control.Button;
-import javafx.scene.control.TextField;
+import javafx.scene.control.*;
 import javafx.stage.Stage;
+import org.example.dimsproject.model.Study;
+import org.example.dimsproject.repository.StudyRepository;
 
 public class StudyController {
 
@@ -39,16 +39,40 @@ public class StudyController {
 
     }
     @FXML
-    void fetch(ActionEvent event){
+    void fetch(ActionEvent event) throws Exception {
+        StudyRepository studyRepository = new StudyRepository();
+        Study s = studyRepository.getStudyById(Integer.parseInt(T1.getText()));
+        if (s.getId()==0){
+            Alert alert = new Alert(Alert.AlertType.INFORMATION, "No such record!", ButtonType.CLOSE);
+            alert.showAndWait();
+            T1.setText("");
+            T1.requestFocus();
+        }
+        T2.setText(s.getTitle());
+        T3.setText(s.getDescription());
 
     }
     @FXML
-    void save(ActionEvent event)
+    void save(ActionEvent event) throws Exception
     {
+        StudyRepository studyRepository = new StudyRepository();
+        Study s = new Study();
+        s.setId(Integer.parseInt(T1.getText()));
+        s.setTitle(T2.getText());
+        s.setDescription(T3.getText());
+        studyRepository.saveNewStudy(s);
 
     }
     @FXML
-    void update(ActionEvent event)
-    {}
+    void update(ActionEvent event) throws Exception
+    {
+        StudyRepository studyRepository = new StudyRepository();
+        Study s = new Study();
+        s.setId(Integer.parseInt(T1.getText()));
+        s.setTitle(T2.getText());
+        s.setDescription(T3.getText());
+        studyRepository.updateStudy(s);
+
+    }
 
     }
