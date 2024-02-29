681d700846d8117aef9f00a1eae7fce84c31e977
Yunus Emre <yunustc5461@gmail.com>
2024-02-29 10:45:20 +0300

    model package syntax fixed.

--- src/main/java/org/example/dimsproject/repository/AdviserRepository.java
+++ src/main/java/org/example/dimsproject/repository/AdviserRepository.java
@@ -1,6 +1,6 @@
 package org.example.dimsproject.repository;
 
-import org.example.dimsproject.Model.Adviser;
+import org.example.dimsproject.model.Adviser;
 
 import java.sql.*;
 public class AdviserRepository {
